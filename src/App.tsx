be22f8456ea707f4f43738efc998ac874295ca36
Priyank <Priyankk2405@gmail.com>
2026-01-30 22:30:21 +0530

    Updated the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { SidebarProvider } from "./components/ui/sidebar";
 import Navbar from "./Dashboard-component/Navbar";
 import AppSidebar from "./Dashboard-component/Sidebar";
+import Homepage from "./Dashboard-component/Homepage";
 
 function App() {
   return (
     <SidebarProvider>
       <div className="flex h-screen">
         <AppSidebar />
-        <div className="flex-1">
+        <div className="flex-1 ml-[1080px]">
           <Navbar />
         </div>
       </div>
+      <div>
+        <Homepage />
+      </div>
     </SidebarProvider>
+  
   );
 }
 
